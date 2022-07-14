897984f3325ed6d7a90b744407c602dc60adf2a3
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-07-14 19:37:29 -0400

    Update candy.py

--- candy.py
+++ candy.py
@@ -1,30 +1,67 @@
-from inventory import add_Candy, remove_Candy, show_Inventory
-from profit import calculate_Profit
+def add_Candy(candyInStock, allowedCandy, allowedCandyListMaxSize):
+    candyType = input("What type of candy are you trying to add? Choose Candy Bar, Gummie, or Popsicle: ")
+    candyFlavor = input("What flavor of candy is it? ")
+
+    candy = {"Candy Type": candyType,
+             "Flavor": candyFlavor}
+
+    if candy in candyInStock:
+        print("We have that candy in stock already")
+    elif candy["Candy Type"] not in allowedCandy:
+        print("We do not stock that type of candy")
+    else:
+        count = 0
+        for x in candyInStock:
+            if x["Candy Type"] == candy["Candy Type"]:
+                count += 1
+        for y in allowedCandyListMaxSize:
+            if y["Candy Type"] == candy["Candy Type"]:
+                maxAmt = y["Max Amt"]
+        if count >= maxAmt:
+            print("We have enough of that candy in stock")
+        else:
+            candyInStock.append(candy)
+            print(candy["Candy Type"] + " " + candy["Flavor"] + " added to inventory.")
+            return candyInStock
+    return candyInStock
 
 
-def main():
-    candyInStock = [{"Candy Type": "Candy Bar", "Flavor": "Chocolate"},
-                    {"Candy Type": "Gummie", "Flavor": "Orange"}]
-    allowedCandy = ["Candy Bar", "Gummie", "Popsicle"]
-    allowedCandyListMaxSize = [{'Candy Type': "Candy Bar", 'Max Amt': 3},
-                               {'Candy Type': "Gummie", 'Max Amt': 4},
-                               {'Candy Type': "Popsicle", 'Max Amt': 5}]
-
-    while True:
-        selection = input(
-            "\nEnter 1 to add candy, 2 to remove candy, 3 to list candy, 4 to calculate cost of all candy, 0 to exit: ")
-        if selection == '1':
-            candyInStock = add_Candy(candyInStock, allowedCandy, allowedCandyListMaxSize)
-        elif selection == '2':
-            candyInStock = remove_Candy(candyInStock)
-        elif selection == '3':
-            show_Inventory(candyInStock)
-        elif selection == '4':
-            calculate_Profit(candyInStock)
-        elif selection == '0':
-            print("Session ended\n")
-            quit()
+def remove_last_candy_added(candyInStock):
+    print("Oops!")
+    candyInStock.pop()
+    return show_Inventory(candyInStock)
 
 
-main()
 
+def remove_Candy(candyInStock):
+    candyType = input("What type of candy is being removed?: ")
+    candyFlavor = input("What flavor of candy is it?: ")
+
+    candyToRemove = {"Candy Type": candyType,
+                     "Flavor": candyFlavor}
+    if candyToRemove not in candyInStock:
+        print("We do not have that candy in stock")
+    else:
+        if candyToRemove in candyInStock:
+            candyInStock.remove(candyToRemove)
+            print("Candy removed.")
+            return candyInStock
+
+
+def show_Inventory(candyInStock):
+    print("Candies in stock: ")
+    count = 0
+    for candy in candyInStock:
+        print(candy)
+        count += 1
+    print(f'Total number: ', count)
+
+
+def clear_Inventory(candyInStock):
+    candyInStock.clear()
+    print("Inventory cleared")
+    print(f'Amount of candies in stock: ', len(candyInStock))
+
+
+candyInStock = []
+allowedCandy = ["Candy Bar", "Popscicle", "Gummie"]
