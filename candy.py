b2e2be31eb1e55f269636811ad27b264476a43b6
Lelah Jones <77903182+lelahjones@users.noreply.github.com>
2022-07-16 10:50:36 -0400

    Update candy.py

--- candy.py
+++ candy.py
@@ -2,31 +2,30 @@ from inventory import *
 
 
 def main():
-    candyInStock = [{"Candy Type": "Candy Bar", "Flavor": "Chocolate"},
-                    {"Candy Type": "Gummie", "Flavor": "Orange"}]
-    allowedCandy = ["Candy Bar", "Gummie", "Popsicle"]
+    candyInStockArray = [{"Candy Type": "Candy Bar", "Flavor": "Chocolate"},
+                         {"Candy Type": "Gummie", "Flavor": "Orange"}]
+    allowedCandyList = ["Candy Bar", "Gummie", "Popsicle"]
     allowedCandyListMaxSize = [{'Candy Type': "Candy Bar", 'Max Amt': 3},
                                {'Candy Type': "Gummie", 'Max Amt': 4},
                                {'Candy Type': "Popsicle", 'Max Amt': 5}]
 
     while True:
         selection = input(
-            "\nEnter \n1 - to add candy \n2 - to remove last candy from inventory \n3 - to remove one candy from inventory \n4 - to list inventory \n5 - to clear inventory \n0 - to exit:\n")
+            "\nEnter \n1 - to add candy \n2 - to remove last candy from inventory \n3 - to remove one candy from "
+            "inventory \n4 - to list inventory \n5 - to clear inventory \n0 - to exit:\n")
         if selection == '1':
-            candyInStock = add_Candy(candyInStock, allowedCandy, allowedCandyListMaxSize)
+            candyInStockArray = add_Candy(candyInStockArray, allowedCandyList, allowedCandyListMaxSize)
         elif selection == '2':
-            candyInStock = remove_last_candy_added(candyInStock)
+            candyInStockArray = remove_last_candy_added(candyInStockArray)
         elif selection == '3':
-            candyInStock = remove_Candy(remove_Candy(candyInStock))
+            candyInStockArray = remove_Candy(remove_Candy(candyInStockArray))
         elif selection == '4':
-            show_Inventory(candyInStock)
+            show_Inventory(candyInStockArray)
         elif selection == '5':
-            clear_Inventory(candyInStock)
+            clear_Inventory(candyInStockArray)
         elif selection == '0':
             print("Session ended\n")
             quit()
 
 
 main()
-
-
